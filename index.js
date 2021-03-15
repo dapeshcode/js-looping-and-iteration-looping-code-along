33b8c910c7fd64913cab6dd034a69ce44954fd16
dapeshcode <ampeccia@gmail.com>
2021-03-14 20:30:52 -0500

    commit tree 4d8c2b5ca6c3b37f930607de6f2195b843c5bbc6

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// write cards 
+function writeCards(names, event) {
+
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+
+function countDown(num) {
+    let count = num;
+    while (count >= 0) {
+    console.log(count);
+    count -= 1;
+    }
+}
+
+
+
